9fbc6b1520e971528fa68228aa069edb42c4ffe4
ChristopherRotter <christopher.rotter@stud.hs-mannheim.de>
2017-05-03 20:09:48 +0200

    Fixed bugs, changed message storing method, added comments

--- code/chatserver/src/var/cnr/chatserver/ChatServer.java
+++ code/chatserver/src/var/cnr/chatserver/ChatServer.java
@@ -1,12 +1,14 @@
 package var.cnr.chatserver;
 
 import java.io.*;
+import java.nio.ByteBuffer;
 import java.nio.file.Files;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
 import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
 import org.codehaus.jackson.*;
@@ -15,12 +17,16 @@ import org.codehaus.jettison.json.*;
 import com.sun.grizzly.http.SelectorThread;
 import com.sun.jersey.api.container.grizzly.GrizzlyWebContainerFactory;
 
+/**
+ * A chat server built on the RESTful API that can receive, store and send messages in JSON format.
+ * @author Christopher Rotter, Nico Gensheimer, Raphael Lubaschewski
+ */
 @Path("")
 public class ChatServer
 {
 	public static void main(String[] args) throws IllegalArgumentException, IOException
 	{
-		final String baseUri = "http://localhost:9998/";
+		final String baseUri = "http://localhost:5000/";
 		final String packageName = "var.cnr.chatserver";
 		final Map<String, String> initParams = new HashMap<String, String>();
 		initParams.put("com.sun.jersey.config.property.packages", packageName);
@@ -35,136 +41,202 @@ public class ChatServer
 		System.exit(0);
 	}
 
+	/**
+	 * Retrieves all available messages for a user, sends them to the client and deletes them.
+	 * @param userId	The user ID of the user.
+	 * @return			A HTTP response with all available messages in JSON format attached.
+	 */
+	@GET
+	@Path("/messages/{user_id}")
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getMessages(@PathParam("user_id") String userId)
+	{
+
+		return getMessages(userId, null);
+	}
+
+	/**
+	 * Retrieves all available messages for a user with a sequence number greater than the parameter sequenceNumber,
+	 * sends them to the client and deletes them.
+	 * @param userId			The user ID of the user.
+	 * @param sequenceNumber	The number of the message that the client received last.
+	 * @return					A HTTP response with all available messages in JSON format attached.
+	 */
 	@GET
 	@Path("/messages/{user_id}/{sequene_number}")
-	@Produces("application/json")
-	public Response getMessage(@PathParam("user_id") String userId, @PathParam("sequence_number") String sequenceNumber)
+	@Produces(MediaType.APPLICATION_JSON)
+	public Response getMessages(@PathParam("user_id") String userId, @PathParam("sequence_number") String sequenceNumber)
 	{
-		List<Message> messages;
+		Message[] messages;
 
 		try
 		{
 			messages = readFile(userId + ".txt");
+
+			if (messages == null)
+			{
+				return Response.status(Response.Status.NO_CONTENT).build();
+			}
 		}
 		catch (Exception e)
 		{
-			return Response.noContent().build();
+			return Response.status(Response.Status.NO_CONTENT).build();
 		}
 
-		List<Message> unreadMessages;
+		Message[] unreadMessages;
 
 		if (sequenceNumber != null)
 		{
 			int sequence = Integer.parseInt(sequenceNumber);
-			unreadMessages = messages.subList(sequence - messages.get(0).getSequence() + 1, messages.size());
+			int unread = messages[messages.length - 1].getSequence() - sequence;
+			unreadMessages = new Message[unread];
+			System.arraycopy(messages, messages.length - unread, unreadMessages, 0, unread);
 		}
 		else
 		{
 			unreadMessages = messages;
 		}
 
-		new File(userId + ".txt").delete();
-		return Response.ok(messagesToJSONArray((Message[]) unreadMessages.toArray())).build();
+		return Response.status(Response.Status.OK).entity(messagesToJSONArray(unreadMessages).toString()).build();
 	}
 
+
+	/**
+	 * Stores the message in a file so the chat partner can retrieve it. If the message was in correct format,
+	 * the server will answer with HTTP code 201, the message's sequence number and the server time, in JSON format.
+	 * @param msg	The message for the chat partner, in JSON format.
+	 * @return		A HTTP response.
+	 */
 	@PUT
 	@Path("/send")
-	@Consumes("application/json")
-	public Response sendMessage(JSONObject object)
+	@Produces(MediaType.APPLICATION_JSON)
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response sendMessage(String msg)
 	{
-		System.out.println("test");
 		try
 		{
-			Message message = new Message(object);
-			String fileName = message.getFrom() + message.getTo() + ".txt";
-			List<Message> messageList = readFile(fileName);
-			messageList.add(message);
-			writeToFile(fileName, (Message[]) messageList.toArray());
+			Message[] message = new Message[1];
+			message[0] = new Message(new JSONObject(msg));
+			String fileName = message[0].getTo() + ".txt";
+			writeToFile(fileName, message);
+			return Response.status(Response.Status.CREATED).entity("").build();
 		}
 		catch (Exception e)
 		{
 			e.printStackTrace();
+			return Response.status(Response.Status.BAD_REQUEST).build();
 		}
-
-		return Response.created(null).build();
 	}
 
+	/**
+	 * Converts Messages to JSONArray.
+	 * @param messages	An array of messages.
+	 * @return			A JSONArray object.
+	 */
 	private JSONArray messagesToJSONArray(Message[] messages)
 	{
 		JSONArray array = new JSONArray();
 
 		for (int i = 0; i < messages.length; i++)
-			array.put(messages[i]);
+		{
+			try
+			{
+				array.put(messages[i].toJSONObject());
+			}
+			catch (JSONException e)
+			{
+				e.printStackTrace();
+			}
+		}
 
 		return array;
 	}
 
-	private List<Message> readFile(String fileName) throws JsonParseException, IOException, ParseException
+	/**
+	 * Reads a file if it exists, converts the data to messages and returns them. The file is then deleted.
+	 * @param fileName		The name of the file.
+	 * @return				An array of messages or null if the file doesn't exist.
+	 * @throws IOException	Thrown when the file can't be read.
+	 */
+	private Message[] readFile(String fileName) throws IOException
 	{
-		List<Message> messageList = new ArrayList<Message>();
-		JsonFactory factory = new JsonFactory();
+		File file = new File(fileName);
 
-		JsonParser parser = factory.createJsonParser(new File(fileName));
-		JsonToken token = parser.nextToken();
-
-		while(token != JsonToken.END_ARRAY)
+		if (file.exists())
 		{
-			String from = null, to = null, text = null;
-			Date date = null;
-			int sequence = -1;
+			byte[] countBytes = new byte[4];
+			FileInputStream inStream = new FileInputStream(fileName);
+			inStream.read(countBytes, 0, 4);
+			int count = ByteBuffer.wrap(countBytes).getInt();
+			byte[][] messageBytes = new byte[count][];
+			Message[] messages = new Message[count];
 
-			while(token!= JsonToken.END_OBJECT)
+			for (int i = 0; i < count; i++)
 			{
-				String fieldName = parser.getCurrentName();
+				byte[] messageLengthBytes = new byte[4];
+				inStream.read(messageLengthBytes, 0, 4);
+				int messageLength = ByteBuffer.wrap(messageLengthBytes).getInt();
+				messageBytes[i] = new byte[messageLength];
+				inStream.read(messageBytes[i], 0, messageLength);
 
-				if(fieldName.equals("from"))
-				{
-					token = parser.nextToken();
-					from = parser.getText();
-				}
-				else if(fieldName.equals("to"))
-				{
-					token = parser.nextToken();
-					to = parser.getText();
-				}
-				else if(fieldName.equals("text"))
-				{
-					token = parser.nextToken();
-					text = parser.getText();
-				}
-				else if(fieldName.equals("date"))
-				{
-					token = parser.nextToken();
-					date = new SimpleDateFormat(Message.ISO8601).parse(parser.getText());
-				}
-				else if(fieldName.equals("sequence"))
-				{
-					token = parser.nextToken();
-					sequence = parser.getIntValue();
-				}
-
-				token = parser.nextToken();
+				messages[i] = Message.Deserialize(messageBytes[i]);
 			}
 
-			parser.close();
-			messageList.add(new Message(from, to, date, text, sequence));
-		}
+			inStream.close();
+			file.delete();
 
-		return messageList;
+			return messages;
+		}
+		else
+		{
+			return null;
+		}
 	}
 
-	private void writeToFile(String fileName, Message[] messages) throws JsonGenerationException, IOException, JSONException
+	/**
+	 * Writes messages into a file. If the file already exists, the messages will be appended.
+	 * @param fileName		The name of the file.
+	 * @param messages		An array of messages.
+	 * @throws IOException	Thrown when the file can't be written.
+	 */
+	private void writeToFile(String fileName, Message[] messages) throws IOException
 	{
-		JsonFactory factory = new JsonFactory();
-		JsonGenerator generator = factory.createJsonGenerator(new File(fileName), JsonEncoding.UTF8);
-		generator.writeStartArray();
 
-		for (int i = 0; i < messages.length; i++)
+		File file = new File(fileName);
+		Message[] existingMessages = new Message[0];
+
+		if (file.exists())
 		{
-			generator.writeObject(messages[i].toJSONObject());
+			existingMessages = readFile(fileName);
 		}
 
-		generator.writeEndArray();
-		generator.close();
+		int count = messages.length + existingMessages.length;
+		int[] messageLengths = new int[count];
+		byte[][] messageBytes = new byte[count][];
+
+		for (int i = 0; i < existingMessages.length; i++)
+		{
+			messageBytes[i] = existingMessages[i].Serialize();
+			messageLengths[i] = messageBytes[i].length;
+		}
+
+		for (int i = existingMessages.length; i < count; i++)
+		{
+			messageBytes[i] = messages[i - existingMessages.length].Serialize();
+			messageLengths[i] = messageBytes[i].length;
+		}
+
+		FileOutputStream outStream = new FileOutputStream(fileName);
+		byte[] countBytes = ByteBuffer.allocate(4).putInt(count).array();
+		outStream.write(countBytes);
+
+		for (int i = 0; i < count; i++)
+		{
+			byte[] messageLengthBytes = ByteBuffer.allocate(4).putInt(messageLengths[i]).array();
+			outStream.write(messageLengthBytes);
+			outStream.write(messageBytes[i]);
+		}
+
+		outStream.close();
 	}
 }
--- code/chatserver/src/var/cnr/chatserver/Message.java
+++ code/chatserver/src/var/cnr/chatserver/Message.java
@@ -1,9 +1,17 @@
 package var.cnr.chatserver;
 
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.OutputStream;
+import java.io.Serializable;
 import java.text.DateFormat;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.List;
 
 import org.codehaus.jettison.json.JSONException;
 import org.codehaus.jettison.json.JSONObject;
@@ -11,7 +19,7 @@ import org.codehaus.jettison.json.JSONObject;
 /**
  * Chat message.
  */
-class Message
+class Message implements Serializable
 {
 	/** String for date parsing in ISO 8601 format. */
 	public static final String ISO8601 = "yyyy-MM-dd'T'HH:mm:ssZ";
@@ -62,13 +70,18 @@ class Message
         this(from, to, date, text, 0);
     }
 
+    /**
+     * Creates a new message from a JSON object.
+     * @param message			The message in JSON format.
+     * @throws ParseException	Thrown when the date can't be parsed.
+     * @throws JSONException	Thrown when the JSON object can't be converted.
+     */
     public Message(JSONObject message) throws ParseException, JSONException
     {
     	from = message.getString("from");
     	to = message.getString("to");
     	date = new SimpleDateFormat(ISO8601).parse(message.getString("date"));
     	text = message.getString("text");
-    	sequence = message.getInt("sequence");
     }
 
     private String getDate()
@@ -91,6 +104,11 @@ class Message
     	return sequence;
     }
 
+    /**
+     * Converts this message into a JSON object.
+     * @return					The JSON object.
+     * @throws JSONException	Thrown when this message can't be converted.
+     */
     public JSONObject toJSONObject() throws JSONException
     {
     	JSONObject obj = new JSONObject();
@@ -112,5 +130,65 @@ class Message
 
         return String.format("{ 'from': '%s', 'to': '%s', 'date': '%s', 'text': '%s'}".replace('\'',  '"'),
                 from, to, sdf.format(new Date()), text);
+    }
+
+    /**
+     * Serializes this message into a byte array.
+     * @return	A byte array.
+     */
+    public byte[] Serialize()
+    {
+    	ByteArrayOutputStream byteStream = new ByteArrayOutputStream();
+    	ObjectOutputStream objStream;
+
+		try
+		{
+			objStream = new ObjectOutputStream(byteStream);
+			objStream.writeObject(this);
+	    	objStream.close();
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+
+    	return byteStream.toByteArray();
+    }
+
+    /**
+     * Deserializes a byte array into a message.
+     * @param data	The byte array.
+     * @return		The deserialized message.
+     */
+    public static Message Deserialize(byte[] data)
+    {
+    	ByteArrayInputStream byteStream = new ByteArrayInputStream(data);
+    	ObjectInputStream objStream;
+
+		try
+		{
+			objStream = new ObjectInputStream(byteStream);
+			Message message;
+
+			try
+			{
+				message = (Message) objStream.readObject();
+				return message;
+			}
+			catch (ClassNotFoundException e)
+			{
+				e.printStackTrace();
+				return null;
+			}
+			finally
+			{
+				objStream.close();
+			}
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+			return null;
+		}
     }
 }
