df712b6c07531b16b9c6723535e9f2597cd9a6b2
Nico Gensheimer <nico-gensheimer@gmx.de>
2017-05-09 20:33:49 +0200

    Register erneuert mit abfrage ob pesudonym und user schon vorhanden ist

--- code/registrationserver/src/var/cnr/registrationserver/RegistrationServer.java
+++ code/registrationserver/src/var/cnr/registrationserver/RegistrationServer.java
@@ -1,17 +1,15 @@
 package var.cnr.regeistrationserver;
 
-import java.io.*;
-import java.nio.ByteBuffer;
-import java.util.*;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-
-import org.codehaus.jettison.json.*;
-
-import com.sun.grizzly.http.SelectorThread;
-import com.sun.jersey.api.container.grizzly.GrizzlyWebContainerFactory;
 
 import org.codehaus.jettison.json.JSONObject;
 
@@ -19,11 +17,14 @@ import com.sun.grizzly.http.SelectorThread;
 import com.sun.jersey.api.container.grizzly.GrizzlyWebContainerFactory;
 
 
+
+
 public class RegistrationServer {
 
 
 		public static void main(String[] args) throws IllegalArgumentException, IOException
 		{
+			
 			final String baseUri = "http://localhost:5002/";
 			final String packageName = "var.cnr.registrationserver";
 			final Map<String, String> initParams = new HashMap<String, String>();
@@ -40,19 +41,44 @@ public class RegistrationServer {
 		}
 
 
+		HashMap<String,String> userPseudoHashMap = new HashMap<>();
+		HashMap<String,String> userPasswordHashMap = new HashMap<>();
+		
+		
 
 		@PUT
 		@Path("/register")
 		@Produces(MediaType.APPLICATION_JSON)
 		@Consumes(MediaType.APPLICATION_JSON)
-		public Response register(JSONObject profil){
+		public Response register(String profil){
 
+			
+//			zu testzwecken
+//			String profiltest = "{ \"user\": \"glatzo\", \"pseudonym\": \"glatze\", \"password\": \"123\"}";
+			
 			try {
-
-				String user = (String) profil.get("user");
-				String pseudonym = (String) profil.get("pseudonym");
-				String password = (String) profil.get("password");
-
+				JSONObject jsnobj = new JSONObject(profil);
+				String user = (String) jsnobj.get("user");
+				String pseudonym = (String) jsnobj.get("pseudonym");
+				String password = (String) jsnobj.get("password");
+				boolean contains = false;
+				
+				  for(String key : userPseudoHashMap.keySet())
+				    {
+					  if (key.contains(user)) {
+						  contains = true;
+						  if (userPseudoHashMap.get(key).contains(pseudonym)) {
+							  contains = true;
+						}
+					
+					}
+				      
+				    }
+				  if (!contains) {
+					  userPasswordHashMap.put(user, password);
+					  userPseudoHashMap.put(user, pseudonym);
+				}
+						
 				return Response.status(Response.Status.OK).build();
 			} catch (Exception e) {
 				// TODO: handle exception
