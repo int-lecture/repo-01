6dee1472768ff857e68e311fe268cd65556c7d0d
gensheimern <nico-gensheimer@gmx.de>
2017-06-06 08:33:44 +0200

    Add files via upload
    AJAX anbindung via js script

---
+++ code/frontend/registrationServer.js
@@ -0,0 +1,29 @@
+$( document ).ready(function() {  
+	
+});
+
+function registerUser(){
+
+
+	
+	var URL = "http://141.19.142.55:5002/register/";
+	var jeisohnObj = {'pseudonym': $("#nm").val(), 'user': $("#usr").val(), 'password': $("#pwd").val() };
+
+        alert(JSON.stringify(jeisohnObj));
+
+        $.ajax({
+			url: URL,
+			type: 'PUT',    
+			data: JSON.stringify(jeisohnObj),
+			contentType: "application/json; charset=utf-8",
+			dataType: 'json',
+			success: function(result) {
+				alert("success?");
+				window.location.href = "SecureMessenger.html";
+			},
+			error: function(xhr, ajaxOptions, thrownError){
+			alert("Fehler!");
+		}
+  	});
+	return false;
+}
