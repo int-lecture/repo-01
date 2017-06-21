b021b638bc48873b596491cc8f47a1acb911edf9
ChristopherRotter <christopher.rotter@stud.hs-mannheim.de>
2017-06-21 15:57:22 +0200

    Bugfixes

--- code/registrationserver/src/var/cnr/registrationserver/RegistrationServer.java
+++ code/registrationserver/src/var/cnr/registrationserver/RegistrationServer.java
@@ -74,11 +74,6 @@ public class RegistrationServer
 		System.out.println("Starte grizzly...");
 		SelectorThread threadSelector = GrizzlyWebContainerFactory.create(baseUri, initParams);
 		System.out.printf("Grizzly läuft unter %s%n", baseUri);
-		System.out.println("[ENTER] drücken, um Grizzly zu beenden");
-		System.in.read();
-		threadSelector.stopEndpoint();
-		System.out.println("Grizzly wurde beendet");
-		System.exit(0);
 	}
 
 	/**
